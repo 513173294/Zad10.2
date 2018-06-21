2913aba44c1a8d80fd749a3da35c35d84c2407a6
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-06-21 21:30:52 +0200

    druga czesc ale nie jestem pewien czy o to chodzilo

--- src/Direction.java
+++ src/Direction.java
@@ -3,18 +3,25 @@ public enum Direction {
     NORTH("polnoc"),
     EAST("wschod"),
     SOUTH("poludnie"),
-    WEST("zachod");
-
+    WEST("zachod"),
+    UNDENTIFIED("nieznany");
     private final String direction;
 
     Direction(String direction) {
         this.direction = direction;
     }
 
-    public String printDirection(){
+    public String printDirection() {
         return this.direction;
     }
 
 
+    public Direction convert(int num) {
+        if (num == 0) return NORTH;
+        if (num == 1) return EAST;
+        if (num == 2) return SOUTH;
+        if (num == 3) return WEST;
 
+        return UNDENTIFIED;
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -6,17 +6,18 @@ public class Test {
         Scanner scanner = new Scanner(System.in);
 
         Direction[] kierunek = Direction.values();
-        for (int i = 0; i < kierunek.length; i++) {
-            System.out.println(kierunek[i]);
+        for (Direction aKierunek : kierunek) {
+            System.out.println(aKierunek);
         }
 
 
         System.out.println("podaj kierunek geogrficzny");
         String desiredDirection = scanner.nextLine();
         Direction direction = Direction.valueOf(desiredDirection);
-
-
-
         System.out.println("ok wiec idziemy na "+ direction.printDirection());
+
+        System.out.println("a gdzie teraz chcesz isc? "+"\n" + "podaj od 0 - 3 ");
+        int desiredDirection2 = scanner.nextInt();
+        System.out.println("ok zmieniamy wieca kierunek na: " + direction.convert(desiredDirection2));
     }
 }
